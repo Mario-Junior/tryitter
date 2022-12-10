e094ad50e8831a5681c07eb51e261d5446a4cec0
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-12-10 09:17:19 -0300

    feat(repository): Post repository

---
+++ Tryitter.Application/Repository/PostRepository.cs
@@ -0,0 +1,68 @@
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Tryitter.Models;
+
+namespace Tryitter.Repository;
+
+public class PostRepository
+{
+  protected readonly TryitterContext _context;
+
+  public PostRepository(TryitterContext context)
+  {
+    _context = context;
+  }
+
+  public async Task<PostGetDTO> CreatePost(PostCreateDTO post)
+  {
+    var newPost = new Post {
+      Text = post.Text,
+      Image = post.Image,
+      Username = post.Username,
+      CreatedAt = DateTime.Now,
+      UpdatedAt = DateTime.Now,
+    };
+    await _context.Posts.AddAsync(newPost);
+    _context.SaveChanges();
+
+    return new PostGetDTO {
+      Id = newPost.Id,
+      Text = newPost.Text,
+      Image = newPost.Image,
+      Username = newPost.Username,
+      CreatedAt = newPost.CreatedAt,
+      UpdatedAt = newPost.UpdatedAt,
+    };
+  }
+
+  public PostGetDTO GetPostById(Guid postId)
+  {
+    Post post = _context.Posts.Find(postId)!;
+    if (post is null) return null!;
+
+    return new PostGetDTO {
+      Id = post.Id,
+      Text = post.Text,
+      Image = post.Image,
+      Username = post.Username,
+      CreatedAt = post.CreatedAt,
+      UpdatedAt = post.UpdatedAt,
+    };
+  }
+
+  public PostGetDTO GetPostByUsernameAndId(string username, Guid postId)
+  {
+    var post = _context.Posts.Where(user => user.Username == username)
+      .Where(postd => postd.Id == postId).FirstOrDefault();
+    if (post is null) return null!;
+
+    return new PostGetDTO {
+      Id = post.Id,
+      Text = post.Text,
+      Image = post.Image,
+      Username = post.Username,
+      CreatedAt = post.CreatedAt,
+      UpdatedAt = post.UpdatedAt,
+    };
+  }
+}
