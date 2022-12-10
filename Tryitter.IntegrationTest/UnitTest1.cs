20ba94a7c0be869f75a774aa4394523b8e994c87
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-12-10 08:16:18 -0300

    feat(solution): creation of solution with projects
    Application and unit and integration Tests projects

---
+++ Tryitter.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Tryitter.IntegrationTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Tryitter.UnitTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Tryitter.UnitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
