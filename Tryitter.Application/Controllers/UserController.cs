c788b33d1c84ab542c77f53a8aeb77112c8d26bf
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-12-10 09:18:47 -0300

    feat(controllers): User controller

---
+++ Tryitter.Application/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Tryitter.Models;
+using Tryitter.Repository;
+using Tryitter.Auth;
+
+namespace Tryitter.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+  private readonly UserRepository _repository;
+
+  public UserController(UserRepository repository)
+  {
+    _repository = repository;
+  }
+
+  [HttpPost]
+  [AllowAnonymous]
+  public IActionResult CreateUser([FromBody] UserCreateDTO user)
+  {
+    var userExists = _repository.GetUserByUsername(user.Username);
+
+    if (userExists is not null)
+      return BadRequest("User already exists with this username");
+      
+    return Created("", _repository.CreateUser(user));
+  }
+
+  [HttpPost("login")]
+  [AllowAnonymous]
+  public IActionResult Login([FromBody] UserLoginDTO user)
+  {
+    var userFound = _repository.LoginValidate(user.Username, user.Password);
+
+    if (userFound is null)
+      return NotFound("Username and/or password invalid");
+    
+    TokenGenerator tokenGenerator = new();
+
+    string token = tokenGenerator.Generate(userFound);
+
+    return Ok(token);
+  }
+
+  [HttpGet("{username}")]
+  [AllowAnonymous]
+  public IActionResult GetUserByUsername(string username)
+  {
+    var user = _repository.GetUserByUsername(username);
+
+    if (user is null) return NotFound("Username not found");
+    return Ok(user);
+  }
+}
