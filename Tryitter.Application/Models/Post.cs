bf071e57e3f4bb18faabd8d72a4f35cd2ac3211c
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-12-10 09:05:17 -0300

    feat(models): Post model

---
+++ Tryitter.Application/Models/Post.cs
@@ -0,0 +1,41 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tryitter.Models;
+
+public class Post
+{
+  [Key]
+  public Guid Id { get; set; }
+  [MaxLength(300, ErrorMessage = "Maximum of {1} characters allowed")]
+  public string Text { get; set; } = default!;
+  [MaxLength(300, ErrorMessage = "Maximum of {1} characters allowed")]
+  public string? Image { get; set; }
+  [ForeignKey("Username")]
+  [MaxLength(15, ErrorMessage = "Maximum of {1} characters allowed")]
+  public string Username { get; set; } = default!;
+  public User User { get; set; } = default!;
+  public DateTime CreatedAt { get; set; } = DateTime.Now;
+  public DateTime UpdatedAt { get; set; } = DateTime.Now;
+}
+
+public class PostCreateDTO
+{
+  [MaxLength(300, ErrorMessage = "Maximum of {1} characters allowed")]
+  public string Text { get; set; } = default!;
+  [MaxLength(300, ErrorMessage = "Maximum of {1} characters allowed")]
+  public string? Image { get; set; }
+  [ForeignKey("Username")]
+  [MaxLength(15, ErrorMessage = "Maximum of {1} characters allowed")]
+  public string Username { get; set; } = default!;
+}
+
+public class PostGetDTO
+{
+  public Guid Id { get; set; }
+  public string Text { get; set; } = default!;
+  public string? Image { get; set; }
+  public string Username { get; set; } = default!;
+  public DateTime CreatedAt { get; set; } = DateTime.Now;
+  public DateTime UpdatedAt { get; set; } = DateTime.Now;
+}
