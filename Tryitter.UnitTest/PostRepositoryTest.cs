78333f4f2f5106a2524049e6982508416c71eddf
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-12-11 15:08:04 -0300

    test(post): GetPostsByUsername repository method

--- Tryitter.UnitTest/PostRepositoryTest.cs
+++ Tryitter.UnitTest/PostRepositoryTest.cs
@@ -36,4 +36,29 @@ public class PostRepositoryTest
         result.Username.Should().Be(usernameExpected);
         result.Text.Should().Be(textExpected);
     }
+
+    public readonly static TheoryData<TryitterContext, string, int> GetPostsByUsernameTestData =
+    new()
+    {
+        {
+            Helpers.GetContextInstanceForTests("GetPostsByUsernameTest"),
+            "test1",
+            4
+        },
+    };
+
+    [Theory(DisplayName = "Get posts by username successfully")]
+    [MemberData(nameof(GetPostsByUsernameTestData))]
+    public async Task GetPostsByUsernameTest(TryitterContext context, string username, int postListLength)
+    {
+        // Arrange
+        context.ChangeTracker.Clear();
+        PostRepository _postRepository = new(context);
+
+        // Act
+        var result = await _postRepository.GetPostsByUsername(username);
+
+        // Assert
+        result.Count().Should().Be(postListLength);
+    }
 }
