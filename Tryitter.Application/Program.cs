1b9001f5a14a9945ee722c33d40a3afc4f99e1d6
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-12-13 19:44:02 -0300

    feat(swagger): improvement to open directly to homepage

--- Tryitter.Application/Program.cs
+++ Tryitter.Application/Program.cs
@@ -72,7 +72,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
-  app.UseSwaggerUI();
+  app.UseSwaggerUI(c =>
+  {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tryitter.Application v1");
+    c.RoutePrefix = string.Empty;
+  });
 }
 
 app.UseHttpsRedirection();
